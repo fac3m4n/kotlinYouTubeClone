0a2539fa811247e213f5898f8037a4d86f5de41d
Kevo <akerimberdi@gmail.com>
2020-08-31 01:05:56 +0300

    Load and Cache Images w/ Picasso

--- app/src/main/java/com/example/kotlinyoutube_clone/MainAdapter.kt
+++ app/src/main/java/com/example/kotlinyoutube_clone/MainAdapter.kt
@@ -1,9 +1,11 @@
 package com.example.kotlinyoutube_clone
 
+import android.icu.number.NumberFormatter.with
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.video_row.view.*
 
 class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomHolderView>(){
@@ -26,6 +28,15 @@ class MainAdapter(val homeFeed: HomeFeed): RecyclerView.Adapter<CustomHolderView
 //        val videoTitle = videoTitles[position]
         val video = homeFeed.videos[position]
         holder.view.textView_video_title.text = video.name
+
+        holder.view.textView_channel_name.text = video.channel.name + " · " + "20K views\n 4 days ago"
+
+        val thumbnailImageView = holder.view.imageView_video_thumb
+        Picasso.get().load(video.imageUrl).into(thumbnailImageView)
+
+        val channelProfileImage = holder.view.imageView_channel_thumb
+        Picasso.get().load(video.channel.profileImageUrl).into(channelProfileImage)
+
 
     }
 }
